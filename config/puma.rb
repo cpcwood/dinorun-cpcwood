d646e56dd10df9b865c2a9b2815e3fc6cc54bb05
cpcwood <cpcwood@outlook.com>
2021-01-26 21:18:48 +0000

    add circleci and kube config

--- config/puma.rb
+++ config/puma.rb
@@ -5,29 +5,23 @@ app_dir = File.expand_path('..', __dir__)
 shared_dir = "#{app_dir}/shared"
 
 # Specifies the `environment` that Puma will run in.
-environment ENV.fetch('RAILS_ENV') { 'production' }
+environment ENV.fetch('RAILS_ENV') { 'development' }
 
 # pidfile and state
 pidfile "#{shared_dir}/pids/puma.pid"
 state_path "#{shared_dir}/pids/puma.state"
 
 # Threads for serving requests
-threads 1, 16
+threads ENV.fetch('PUMA_STARTING_THREADS') { 1 }, ENV.fetch('PUMA_MAX_THREADS') { 1 }
 
 # Workers (cpu cores)
-workers ENV.fetch('WEB_CONCURRENCY') { 1 }
+workers ENV.fetch('PUMA_WEB_CONCURRENCY') { 1 }
 preload_app!
 
-# Unix socket to for nginix reverse proxy
-# bind "unix://#{shared_dir}/sockets/puma.sock"
-
-# Port for local server use
-port ENV.fetch('PORT') { 3000 }
+# Port
+port 5000
 
 # Debugging
 debug
-
-# Logging
-# stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
 activate_control_app
