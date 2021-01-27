fe87e087632d407487f42f59411c8fec88873b0a
cpcwood <cpcwood@outlook.com>
2021-01-27 11:19:00 +0000

    refactor js

--- app/javascript/packs/homepage_interface/generateCanvas.js
+++ app/javascript/packs/homepage_interface/generateCanvas.js
@@ -1,12 +1,21 @@
 function generateCanvas() {
   var canvasContainer = document.querySelector('#canvas_container')
+  var canvasDimensions = calculateCanvasDimensions()
+  canvasContainer.innerHTML = `<canvas id='canvas' width='${canvasDimensions.width}' height='${canvasDimensions.height}'></canvas>`
+}
+
+function calculateCanvasDimensions() {
   var canvasHeight = window.innerHeight
   var canvasWidth = window.innerWidth
   var maxWidth = Math.floor((canvasHeight/9)*20)
   canvasWidth = Math.min(canvasWidth, maxWidth)
   var maxHeight = Math.floor((canvasWidth/4)*3)
   canvasHeight = Math.min(canvasHeight, maxHeight) 
-  canvasContainer.innerHTML = `<canvas id='canvas' width='${canvasWidth}' height='${canvasHeight}'></canvas>`
+  return {
+    width: canvasWidth,
+    height: canvasHeight
+  }
 }
+
 
 export { generateCanvas }
